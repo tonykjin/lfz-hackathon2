002d557f7fa5bd4bc7783d5b6f6c75a733efc95e
raul jauregui <felx90@outlook.com>
2019-06-18 15:53:46 -0700

    class quote

--- quote.js
+++ quote.js
@@ -1,28 +1,29 @@
-$(document).ready(initializeApp);
 
-function initializeApp() {
-    $(".btn-success").click(function(){
-        randomQuote();
-    });
+    var random = new Quote();
+    random.randomQuote();
 
+class Quote {
+
+    constructor(){
+
+        $(".btn-success").on("click",this.randomQuote);
+    }
+     randomQuote() {
+        $.ajax
+        ({
+            url: "https://api.forismatic.com/api/1.0/?",
+            dataType: "jsonp",
+            data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+            success: function( response) {
+                console.log(response);
+                $("#createQuote").html("<p id='createQuote' class='lead text-center'>" +
+                    response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
+
+                $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
+                    ' (' + response.quoteAuthor + ')');
+            }
+        });
+    }
 }
-function randomQuote() {
-    $.ajax
-    ({
-        url: "https://api.forismatic.com/api/1.0/?",
-        dataType: "jsonp",
-        data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
-        success: function( response ) {
-            console.log(response);
-            $("#createQuote").html("<p id='createQuote' class='lead text-center'>" +
-                response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
 
-            $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
-                ' (' + response.quoteAuthor + ')');
-        }
-    });
-}
-console.log($.ajax);
-$(function() {
-    randomQuote();
-});
+
